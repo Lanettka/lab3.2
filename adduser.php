cb5990194fb63b0f5692642bfd11aad0cc2f3944
calculator1 <alenyhka18@yandex.ru>
2015-04-16 13:55:57 +0300

    Update adduser.php

--- adduser.php
+++ adduser.php
@@ -1,19 +1,13 @@
 <?php
-echo 'You enter following information about yourself:'.'<br>';
-echo"<br>Name:</br>".$_GET['name']."<br>";
-echo"<br>Surname:</br>".$_GET['surname']."<br>";
-echo"<br>Address:</br>".$_GET['address']."<br>";
-echo"<br>Nickname:</br>".$_GET['nickname']."<br>";
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$nik = $_POST['nik'];
+$password = $_POST['password'];
+$confpassword = $_POST['confpassword'];
+$country = $_POST['country'];
+$city= $_POST['city'];
 
-?>
-	<?
-echo'<b>File was save on server name</b>'.
-	$_FILES['userfile']['tmp_name'].'<br>';
-echo'<b>On your mashin he names:</b>'.
-	$_FILES['userfile']['name'].'<br>';
-echo'<b>File size is:</b>'.
-	$_FILES['userfile']['size'].'<br>';
-echo'<b>Mime type of file:</b>'.
-	$_FILES['userfile']['type'].'<br>';
-move_uploaded_file($_FILES['userfile']['tmp_name'],"images/".$_FILES['userfile']['name']);
+echo $name."<br />".$surname."<br />".$nik."<br />".$password."<br />".$country."<br />".$city."<br />";
+}
 ?>
